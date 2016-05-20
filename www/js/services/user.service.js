bf0e8145456e21a2af52bcd1856ba3426babcdcc
Vasily Crespo <vasilycrespo@gmail.com>
2016-05-20 18:15:16 -0400

    Added start files

---
+++ www/js/services/user.service.js
@@ -0,0 +1,15 @@
+angular.module('app.user', []).service('UserService', function() {
+
+  var setUser = function(user_data) {
+    window.localStorage.starter_facebook_user = JSON.stringify(user_data);
+  };
+
+  var getUser = function(){
+    return JSON.parse(window.localStorage.starter_facebook_user || '{}');
+  };
+
+  return {
+    getUser: getUser,
+    setUser: setUser
+  };
+});
